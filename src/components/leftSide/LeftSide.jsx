3772521d57c503f3cf1f680fd71cffb761c9824d
jadepegasus <jadepegasus235@gmail.com>
2023-10-29 22:03:09 +0700

    first commit by 20205013

---
+++ src/components/leftSide/LeftSide.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LogoSearch from '../logoSearch/LogoSearch'
+import './LeftSide.css'
+const LeftSide = () => {
+    return (
+        <div className="LeftSide">
+            <LogoSearch />
+
+        </div>
+    )
+}
+
+export default LeftSide
